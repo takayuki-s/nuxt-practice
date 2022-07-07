833c56b25d6e374b86fdde7e02eb301c3f7b367b
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-07-08 06:36:48 +0900

    store関連を別ファイルへ移動

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,11 @@
 <template>
   <section class="container">
     <p>{{ message }}</p>
-    <p>{{ $store.state.message }}</p>
-    <!-- <button v-on:click="$store.commit('updateMessage')">Update</button> -->
-    <!-- <button v-on:click="$store.commit('updateMessage', 'Commit with payload')">Update</button> -->
-    <!-- <button v-on:click="$store.dispatch('updateMessageAction')">Dispatch</button> -->
-    <button v-on:click="$store.dispatch('updateMessageAction', 'Dispatch with payload')">Dispatch</button>
     <hr>
     <router-link to="/price">Price Page</router-link>
     <router-link to="/axios">Axios Page</router-link>
     <router-link to="/image">Image Page</router-link>
+    <router-link to="/store">Store Page</router-link>
   </section>
 </template>
  
---
+++ pages/store.vue
@@ -0,0 +1,14 @@
+<template>
+  <section class="container">
+    <div>
+      <p>{{ $store.state.message }}</p>
+      <button v-on:click="$store.dispatch('updateMessageAction', 'Dispatch with payload')">Dispatch</button>
+    </div>
+    <router-link to="/">Top Page</router-link>
+  </section>
+</template>
+
+<script>
+export default {
+}
+</script>
